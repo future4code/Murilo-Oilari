9a8bbbacdccaf965c0d513836a080c46227c729a
Murilo Oliari Ribeiro <murilo.oliari@gmail.com>
2020-06-22 16:54:12 -0300

    exercicíos ts

---
+++ semana14/exerciciosTS/src/exe02.js
@@ -0,0 +1,8 @@
+//a.
+function doisNumeros(a, b) {
+    console.log('soma', a + b);
+    console.log('subtração', a - b);
+    a > b ? console.log(a, ' é maior') : console.log(b, 'é maior');
+}
+;
+doisNumeros(2, 5);
---
+++ semana14/exerciciosTS/src/exe04.ts
@@ -0,0 +1,2 @@
+//a.
+const lista: any = []
